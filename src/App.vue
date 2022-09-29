50bed4e4de4eb5274cd9d0470a0e53f939596db1
unknown <joaquinlmartin@gmail.com>
2022-09-29 16:34:05 +0200

    iterations 2-6 bonus done

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,60 @@
 <template>
   <h1>Iron Contacts</h1>
+  <button @click="addContact" class="btn">Add Random Contact</button>
+  <button @click="sortByPopularity" class="btn">Sort by popularity</button>
+  <button @click="sortByName" class="btn">Sort by name</button>
   <table>
     <tr class="first-TR">
-      <th>
-        <h2>Picture</h2>
-      </th>
-      <th>
-        <h2>Name</h2>
-      </th>
-      <th>
-        <h2>Popularity</h2>
-      </th>
-      <th>
-        <h2>Won Oscar</h2>
-      </th>
-      <th>
-        <h2>Won Emmy</h2>
-      </th>
-       <th>
-        <h2>Actions</h2>
-      </th>
-    </tr>
-    <tr v-for="item in contacts" :key="item.id" class="second-TR">
-    <td><img :src="item.pictureUrl" class="image" alt="" /></td>
+      <th><h2>Picture</h2></th>
+      <th><h2>Name</h2></th>
+      <th><h2>Popularity</h2></th>
+      <th><h2>Won Oscar</h2></th>
+      <th><h2>Won Emmy</h2></th>
+      <th><h2>Actions</h2></th></tr>
+    <tr v-for="item in contactsShown" :key="item.id" class="second-TR">
+    <td><img :src="item.pictureUrl" class="image" alt="image" /></td>
     <td>{{item.name}}</td>
     <td>{{item.popularity}}</td>
+    <td><p v-if="item.wonOscar">🏆</p></td>
+    <td><p v-if="item.wonEmmy">🏆</p></td>
+    <td><button @click="() => deleteContact(item.id)">Delete</button></td>
     </tr>
   </table>
 </template>
 
-<script>
-import contacts from './contacts.json';
+<script setup>
+import { ref } from 'vue';
+import contactsData from './contacts.json';
 
-export default {
-  data() {
-    return {
-      contactsBD: contacts,
-      contactsSize: 5,
-      contacts: [
-        {
-          name: 'Idris Elba',
-          pictureUrl: 'https://image.tmdb.org/t/p/w500/d9NkfCwczP0TjgrjpF94jF67SK8.jpg',
-          popularity: 11.622713,
-          id: '11731993-0604-4bee-80d5-67ad845d0a38',
-          wonOscar: false,
-          wonEmmy: false,
-        },
-        {
-          name: 'Johnny Depp',
-          pictureUrl: 'https://image.tmdb.org/t/p/w500/kbWValANhZI8rbWZXximXuMN4UN.jpg',
-          popularity: 15.656534,
-          id: '7dad00f7-3949-477d-a7e2-1467fd2cfc06',
-          wonOscar: false,
-          wonEmmy: false,
-        },
-        {
-          name: 'Monica Bellucci',
-          pictureUrl: 'https://image.tmdb.org/t/p/w500/qlT4904d8oi2NIs28RrgnIZDFZB.jpg',
-          popularity: 16.096436,
-          id: '0ad5e441-3084-47a1-9e9b-b917539bba71',
-          wonOscar: false,
-          wonEmmy: false,
-        },
-        {
-          name: 'Gal Gadot',
-          pictureUrl: 'https://image.tmdb.org/t/p/w500/34kHAyBaBhq2kUrmhM15paEBuuI.jpg',
-          popularity: 10.049256,
-          id: 'b497e3c4-50bb-4ae2-912f-eb36802c5bc2',
-          wonOscar: false,
-          wonEmmy: false,
-        },
-        {
-          name: 'Ian McKellen',
-          pictureUrl: 'https://image.tmdb.org/t/p/w500/coWjgMEYJjk2OrNddlXCBm8EIr3.jpg',
-          popularity: 10.070132,
-          id: '0067ae32-97b6-4431-898e-eb1c10150abb',
-          wonOscar: false,
-          wonEmmy: false,
-        },
-      ],
-    };
-  },
-  components: { },
+const contactsShown = ref(contactsData.slice(0, 5));
+const contactsLeft = ref(contactsData.slice(6, contactsData.length));
+
+const addContact = () => {
+  const randomInt = Math.floor(Math.random() * contactsLeft.value.length);
+  const newContact = contactsLeft.value[randomInt];
+  contactsLeft.value.splice(randomInt, 1);
+  contactsShown.value.unshift(newContact);
+};
+
+const sortByPopularity = () => {
+  contactsShown.value.sort((a, b) => {
+    if (a.popularity < b.popularity) { return 1; }
+    if (a.popularity > b.popularity) { return -1; }
+    return 0;
+  });
+};
+
+const sortByName = () => {
+  contactsShown.value.sort((a, b) => {
+    if (a.name < b.name) { return -1; }
+    if (a.name > b.name) { return 1; }
+    return 0;
+  });
+};
+
+const deleteContact = (id) => {
+  const filteredContacts = contactsShown.value.filter((contact) => contact.id !== id);
+  contactsShown.value = filteredContacts;
 };
 </script>
 
@@ -99,7 +72,11 @@ export default {
   width: auto;
 }
 
-btn {
+.btn {
+ color: white;
+ background-color: rgb(194, 161, 14);
+ border-radius: 1rem;
+ border-width: 0;
  margin: 8px;
 }
 
@@ -117,7 +94,7 @@ nav a.router-link-exact-active {
 }
 
 h1{
-  color: gold;
+  color: rgb(194, 161, 14);
 
 }
 
